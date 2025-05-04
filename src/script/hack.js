a21b54bf3f13fab36e1fa04ee1b3749f72e6f666
Mathys <sammy12778309@gmail.com>
2025-05-04 23:09:29 +0200

    add hack function

--- src/script/hack.js
+++ src/script/hack.js
@@ -1,5 +1,6 @@
-function hack(hack_id){
-    const payload = {
-        
-    }
-}
+function earn_cookies(cookies_to_earn){Game.Earn(cookies_to_earn)}
+function change_cookies(new_cookies_number){Game.cookies = new_cookies_number}
+function infinite_cookies(){Game.cookies = 10 ** 303}
+function reset_cookies(){Game.cookies = 0}
+function earn_sugar_lumps(sugar_lumps_to_earn){Game.lumps = sugar_lumps_to_earn}
+function show_earned_cookies(){alert(Game.cookiesEarned)}
